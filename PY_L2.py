89b2a8324d46c28dd6ad1de62a428a05345a6795
Vladimir Kuzmin <vlasek@mail.ru>
2023-01-28 12:31:26 +0300

    py

---
+++ PY_L2.py
@@ -0,0 +1,6 @@
+a = 5
+print (type(a), id(a))
+a = 'hello'
+print (type(a), id(a))
+a = 42.0 * 3.1415926
+print (type(a), id(a))
---
+++ test.py
@@ -0,0 +1,5 @@
+
+string = 'Программисты не спят'
+words = string.split()
+
+print(f'Время 23:5{6 + len(words)}')
